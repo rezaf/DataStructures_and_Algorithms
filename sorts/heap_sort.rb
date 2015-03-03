c21119f8b5988d8bf434a699fce6c17199a49134
rezaf <reza.farajian@gmail.com>
2015-03-03 11:03:27 -0800

    heap sort syntax updated

--- sorts/heap_sort.rb
+++ sorts/heap_sort.rb
@@ -2,13 +2,13 @@ require_relative '../bin_min_heap'
 
 # Time: O(nlog(n)), Space: O(1)
 def heap_sort(arr)
-  2.upto(arr.length - 1).each do |heap_sz|
-    BinaryMinHeap.heapify_up(arr, heap_sz - 1, heap_sz)
+  2.upto(arr.length - 1).each do |heap_size|
+    BinaryMinHeap.heapify_up(arr, heap_size - 1, heap_size)
   end
 
-  arr.length.downto(2).each do |heap_sz|
-    arr[heap_sz - 1], arr[0] = arr[0], arr[heap_sz - 1]
-    BinaryMinHeap.heapify_down(arr, 0, heap_sz - 1)
+  arr.length.downto(2).each do |heap_size|
+    arr[heap_size - 1], arr[0] = arr[0], arr[heap_size - 1]
+    BinaryMinHeap.heapify_down(arr, 0, heap_size - 1)
   end
 
   arr.reverse
