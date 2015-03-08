5943a347aae7735ad2f2a4cf20a73b5059fb6725
rezaf <reza.farajian@gmail.com>
2015-03-08 10:53:20 -0700

    updated bfs to be more general

--- graphs/bfs.rb
+++ graphs/bfs.rb
@@ -1,20 +1,19 @@
-require_relative 'undirected_graph'
-
-class vertex
-def bfs(graph, node)
-  queue = [node]
-  node.discovered = true
+def bfs(vertex)
+  queue = [vertex]
+  vertex.discovered = true
 
   until queue.empty?
-    node = queue.shift
+    vertex = queue.shift
 
+    vertex.edges.each do |edge|
+      other_vertex = edge.vertices.select do |test_vertex|
+        test_vertex != vertex
+      end
+
+      unless other_vertex.discovered
+        queue << other_vertex
+        other_vertex.discovered = true
+      end
+    end
   end
-  # until nodes.empty?
-  #   node = nodes.shift
-  #
-  #   return node if node == target_node
-  #   nodes.concat(node.children)
-  # end
-  #
-  # nil
 end
