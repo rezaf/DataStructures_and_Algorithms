40505a5ad6700a1bc1cde4cfc6c9817f9ba99cf1
rezaf <reza.farajian@gmail.com>
2015-03-07 19:34:46 -0800

    directed graphs added

---
+++ graphs/directed_graph.rb
@@ -0,0 +1,32 @@
+class Vertex
+  attr_reader :value, :in_edges, :out_edges
+
+  def initialize(value)
+    @value = value
+    @in_edges = []
+    @out_edges = []
+  end
+end
+
+class Edge
+  attr_reader :from_vertex, :to_vertex, :cost
+
+  def initialize(from_vertex, to_vertex, cost = 1)
+    self.from_vertex = from_vertex
+    self.to_vertex = to_vertex
+    self.cost = cost
+
+    from_vertex.out_edges << self
+    to_vertex.in_edges << self
+  end
+
+  def destroy!
+    self.from_vertex.out_edges.delete(self)
+    self.from_vertex = nil
+    self.to_vertex.in_edges.delete(self)
+    self.to_vertex = nil
+  end
+
+  protected
+  attr_writer :from_vertex, :to_vertex, :cost
+end
