fd5860befefab6bba443fd79482dd4775ab6447f
rezaf <reza.farajian@gmail.com>
2015-03-08 14:56:00 -0700

    dijkstra shortest path added

--- graphs/dijkstra.rb
+++ graphs/dijkstra.rb
@@ -1,23 +1,35 @@
-require_relative '../bin_min_heap'
+require_relative '../priority_map'
 require_relative 'directed_graph'
 
-def dijkstra(vertices, source)
-  queue = BinaryMinHeap.new
-  source.distance = 0
+def dijkstra(source)
+  locked_in_paths = {}
+  possible_paths = PriorityMap.new
+  possible_paths[source] = { cost: 0, last_edge: nil }
 
-  vertices.each do |vertex|
-    unless vertex == source
-      vertex.distance = Float::INFINITY
-      vertex.prev = nil
-    end
-    queue.push(vertex)
+  until possible_paths.empty?
+    vertex, data = possible_paths.extract
+    locked_in_paths[vertex] = data
+
+    update_possible_paths(vertex, locked_in_paths, possible_paths)
   end
 
-  until queue.empty?
-    vertex = queue.extract
+  locked_in_paths
+end
 
-    vertex.neighbors.each do |neighbor|
-      alt = vertex.distance + length
-    end
+def update_possible_paths(vertex, locked_in_paths, possible_paths)
+  path_to_vertex_cost = locked_in_paths[vertex][:cost]
+
+  vertex.out_edges.each do |edge|
+    to_vertex = edge.to_vertex
+    next if locked_in_paths.has_key?(to_vertex)
+
+    extended_path_cost = path_to_vertex_cost + edge.cost
+    next if possible_paths.has_key?(to_vertex) &&
+            possible_paths[to_vertex][:cost] <= extended_path_cost
+
+    possible_paths[to_vertex] = {
+      cost: extended_path_cost,
+      last_edge: edge
+    }
   end
 end
--- graphs/topo_sort.rb
+++
@@ -1,9 +0,0 @@
-def topo_sort(nodes)
-  sorted_nodes = []
-
-  nodes.each do |node|
-    visit(node) unless node.discovered
-  end
-
-  sorted_nodes
-end
