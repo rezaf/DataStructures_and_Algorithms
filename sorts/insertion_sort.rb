41e7f26e25e3f3d35ba6fe737b6629a0e3837eaa
rezaf <reza.farajian@gmail.com>
2015-03-17 15:43:04 -0700

    syntax updated for insertion sort

--- sorts/insertion_sort.rb
+++ sorts/insertion_sort.rb
@@ -1,10 +1,10 @@
 # Time: O(n**2), Space: O(1)
 def insertion_sort(arr)
   arr.length.times do |i|
-    j = i  
+    j = i
     while j > 0 && arr[j - 1] > arr[j]
       arr[j], arr[j - 1] = arr[j - 1], arr[j]
-      j = j - 1
+      j -= 1
     end
   end
 
